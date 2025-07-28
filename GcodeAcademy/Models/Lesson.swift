c561aeb3874b80d13147a8a0d07ed9c57d7b6f83
Alana <gcodealanadavis@gmail.com>
2025-07-28 13:15:53 -0400

    Added Lessons, User and MockData.

--- GcodeAcademy/Models/Lesson.swift
+++ GcodeAcademy/Models/Lesson.swift
@@ -4,4 +4,10 @@
 //
 //  Created by Estuardo Lopez Letona on 7/28/25.
 //
+import Foundation
 
+struct Lesson: Identifiable {
+    var id = UUID()
+    var title: String
+    var description: String
+}
--- GcodeAcademy/Resources/MockData.swift
+++ GcodeAcademy/Resources/MockData.swift
@@ -4,4 +4,15 @@
 //
 //  Created by Estuardo Lopez Letona on 7/28/25.
 //
+import Foundation
 
+struct MockData {
+    // Sample lessons
+    static let lessons: [Lesson] = [
+        Lesson(title: "Introduction to Swift", description: "Learn the basics of Swift"),
+        Lesson(title: "Variables and Constants", description: "Understanding data storage")
+    ]
+    
+    // Sample user
+    static let currentUser = User(name: "Gcode Student")
+}
