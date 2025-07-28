9b861c43a994610c8fce9de3d5ed382379a2a086
Brandon <gcodebrandonsteide@gmail.com>
2025-07-28 13:24:00 -0400

    Create the chat bot interface.

--- GcodeAcademy/Views/Chat/ChatBotView.swift
+++ GcodeAcademy/Views/Chat/ChatBotView.swift
@@ -1,7 +1,40 @@
-//
-//  ChatBotView.swift
-//  GcodeAcademy
-//
-//  Created by Estuardo Lopez Letona on 7/28/25.
-//
+import SwiftUI
 
+struct ChatBotView: View {
+    @State private var messageText = ""
+    
+    var body: some View {
+        VStack {
+            // Message display area
+            ScrollView {
+                VStack(alignment: .leading) {
+                    Text("Hi! I'm the Gcode Helper Bot. How can I assist you?")
+                        .padding()
+                        .background(Color.gray.opacity(0.2))
+                        .cornerRadius(10)
+                }
+                .padding()
+            }
+            
+            // Input field
+            HStack {
+                TextField("Type a message...", text: $messageText)
+                    .padding()
+                    .background(Color.gray.opacity(0.1))
+                    .cornerRadius(10)
+                
+                Button(action: {
+                    // Send message functionality will go here
+                }) {
+                    Image(systemName: "arrow.up.circle.fill")
+                        .font(.system(size: 30))
+                }
+            }
+            .padding()
+        }
+    }
+}
+
+#Preview {
+    ChatBotView()
+}
