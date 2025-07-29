68726050ad85fb51eb650e8ac9945a9387c02cbb
Brandon <gcodebrandonsteide@gmail.com>
2025-07-29 11:36:23 -0400

    Implement the ProfileInfoSection component

--- GcodeAcademy/Views/Profile/ProfileInfoSection.swift
+++ GcodeAcademy/Views/Profile/ProfileInfoSection.swift
@@ -1,13 +1,25 @@
 import SwiftUI
 
-// Displays app information at the bottom of the profile
 struct ProfileInfoSection: View {
-    var body: some View {
-        // Placeholder implementation
-        Text("Here goes the info section")
+    var body : some View {
+        VStack(spacing: 8) {
+            // App name
+            Text("Gcode Academy")
+                .font(.bodyMedium)
+                .foregroundColor(.textSecondaryApp)
+            
+            
+            // App version
+            Text("Version 1.0")
+                .font(.caption)
+                .foregroundColor(.textTertiaryApp)
+            
+        }
+        .frame(maxWidth: .infinity)
+        .padding(.top, 8)
+        .padding(.bottom, 30)
     }
 }
-
 #Preview {
     ProfileInfoSection()
 }
