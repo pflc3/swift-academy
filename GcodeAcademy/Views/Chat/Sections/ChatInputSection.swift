d7ef25344955e8f8e69b50d644a722039cc373bf
Brandon <gcodebrandonsteide@gmail.com>
2025-07-31 11:23:56 -0400

    Put in strucutre and placeholder files.

--- GcodeAcademy/Views/Chat/Sections/ChatInputSection.swift
+++ GcodeAcademy/Views/Chat/Sections/ChatInputSection.swift
@@ -1,1 +1,19 @@
+import SwiftUI
 
+// Provides an input field and send button for typing and sending messages
+struct ChatInputSection: View {
+    var body: some View {
+        HStack {
+            Text("Message Input Placeholder")
+                .padding()
+                .background(Color.surfaceApp)
+                .cornerRadius(8)
+        }
+        .padding()
+        .background(Color.cardBackgroundApp)
+    }
+}
+
+#Preview {
+    ChatInputSection()
+}
