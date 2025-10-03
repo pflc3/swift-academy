c9c5fe8f608fb43a15f1bddf4486b59c3bfcdfc0
Estu <estulpzlet@gmail.com>
2025-10-03 00:54:25 -0400

    fix(splashview): changed splash to plain white to look good with diff phone sizes, last splash did not match launch screen on certain phone sizes

--- SwiftAcademy/App/SplashView.swift
+++ SwiftAcademy/App/SplashView.swift
@@ -2,27 +2,7 @@ import SwiftUI
 
 struct SplashView: View {
     var body: some View {
-        ZStack {
-            Color(.systemBackground).ignoresSafeArea()
-
-            VStack {
-                Spacer()
-
-                Image("Logo")
-                    .resizable()
-                    .scaledToFit()
-                    .frame(width: 393, height: 393)
-                    .padding(.bottom, -76.15)
-
-                Text("Swift Academy")
-                    .font(.custom("AmericanTypewriter-Bold", size: 40))
-                    .foregroundColor(Color(hex: "#38b6ff"))
-                    .padding(.bottom, 66)
-
-                Spacer()
-                Spacer()
-            }
-        }
+        Color(.systemBackground).ignoresSafeArea()
     }
 }
 
